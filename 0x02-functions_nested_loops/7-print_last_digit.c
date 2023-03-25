5b1353832c1d3509aff8960a1e73ef036425297f
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-03-25 08:36:42 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,13 @@
 
 int print_last_digit(int a)
 {
-	int lastDigit:
+	int lastDigit;
 	
 	if (n < 0)
 		lastDigit = -1 * (n % 10);
 	else
 		lastDigit = n % 10;
+
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
