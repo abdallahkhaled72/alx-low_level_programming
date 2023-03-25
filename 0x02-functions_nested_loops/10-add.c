67007d3382d08bcd81b83c831fe1f40b67b4e6ae
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-03-25 09:06:59 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * return: sum of a 7 b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
