caa48612a007d155d1d023ab6ba1dc27c0fa19c1
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-03-25 19:19:29 -0700

    done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * pirnt_most_numbers - print 0 - 9 apart
+ * print_most_numbers - print 0 - 9 apart
  *		from 2 and 4 and you can
  *		only use _putchar twice
  *
@@ -12,7 +12,7 @@ int print_most_numbers(void)
 {
 	int num:
 
-	for(num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num == 2 || num == 4)
 			continue;
