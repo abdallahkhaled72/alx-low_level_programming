8c1f202cb94d1fe33a2f6fb16b0b72b8f6b96a8d
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-03-28 13:36:03 -0700

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,8 @@
 /**
  * *_strcpy - copies the string pointed to by src
  *
- * &dest: char type string
- * &src: char type string
+ * *dest: char type string
+ * *src: char type string
  * Description: copy the string pointed to by pointer `scr` to
  * the buffer pointed to by `dest`
  * Return: Pointer to `dest`
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	int i = -1;
 
 	do {
-	
+
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
