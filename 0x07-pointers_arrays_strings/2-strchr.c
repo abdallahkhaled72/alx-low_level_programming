1cbfc4cf12ee5ae9192c451d24fcb3d52641ab45
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-03 17:41:14 -0700

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strchr - replace a spicific character with another one
+ * @s: pointer to put the constant
+ * @c: constant
+ *
+ * Retrun: pointer to s
+*/
+
+char *_strchr(char *s, char c)
+{
+	int num;
+
+	for(num = 0; s[num] >= '\0'; num++)
+	{
+		if (s[num] == c)
+		{
+			return (s + num);
+		}
+	}
+	return ('\0');
+}
