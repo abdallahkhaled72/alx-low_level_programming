b79ccf783cadfe74655e5ee1d5e096bcc5ac29ca
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-10 21:39:20 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 /**
- *
+ * main - prints integer number followed by new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
 */
 
-
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%d\n", argv[0]);
+	return (0);
+}
