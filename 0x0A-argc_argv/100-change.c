19cb590d34b912e5088cf46b968c0be968ebbc54
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-10 20:18:48 -0700

    done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to change for
+ * an amount of money
+ * @argc: number of commandline arguments
+ * @argv: pointer to an array of command line at arguments
+ * Return: 0 (success) or NULL (fail)
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int y, changer = 0, amount = argv[1];
+		int base[] = {25, 10, 5, 2, 1};
+
+		for (y = 0; y < 5; y++)
+		{
+			if (money >= base[i])
+			{
+				changer += amount / base[i];
+				amount = amount % base[i];
+				if (amount % base[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", changer);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
