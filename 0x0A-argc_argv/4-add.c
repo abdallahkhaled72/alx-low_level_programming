3c06c638a28cd00f17967d477d0563ea55b62424
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-10 22:35:51 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,7 @@
  * main - print the summation of numbers
  * @argc: int
  * @argv: list
- * Return: Always 0
+ * Return: integer number or Error or 0
 */
 
 int main(int argc, char **argv)
@@ -13,22 +13,13 @@ int main(int argc, char **argv)
 	int add = 0;
 	char *check;
 
-	if (argc == 0)
+	while (--argc)
 	{
-		printf("0\n");
+		for (check = argv[argc]; *check; check++)
+			if (*check < 48 || *check > 58)
+				return (printf("Error\n"), 1);
+		add += atoi(argv[argc]);
 	}
-	else
-	{
-		while (--argc)
-		{
-			for (check = argv[argc]; *check; check++)
-			{
-				if (*check < 48 || *check > 58)
-					return (printf("Error\n"), 1);
-				add += atoi(argv[argc]);
-			}
-		}
-		printf("%d\n", add);
-	}
+	printf("%d\n", add);
 	return (0);
 }
