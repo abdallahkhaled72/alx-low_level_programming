97f88c0649917e9e5d1fafb7835eee7572857104
Abdullah Khaled <abdullakhalid7857166@gmail.com>
2023-04-10 21:59:13 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print multiplication result of two numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0 if success, non if fail
+*/
+
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
